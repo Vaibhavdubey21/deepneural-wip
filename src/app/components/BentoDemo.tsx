91cf3cd713dad0366d4c7f49717ad5c7f85c795c
Vivek Saraf <vivek396@gmail.com>
2026-01-24 10:37:31 +0530

    chnages in the AI saas

--- src/app/components/BentoDemo.tsx
+++ src/app/components/BentoDemo.tsx
@@ -25,19 +25,21 @@ const features = [
     className: 'lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-4',
   },
   {
-    Icon: GlobeIcon,
-    name: 'AI SaaS Dental Imaging Platform',
-    description:
-      'Revolutionizing dental imaging with AI-powered diagnostics that detect issues with 98% accuracy, helping dentists provide better care and reduce misdiagnosis.',
-    href: '/contact-us',
-    cta: 'Learn more',
-    badgeClass: 'bg-red-500 dark:bg-red-600',
-    badgeLabel: 'Healthcare',
-    BadgeIcon: HeartIcon,
-    companyName: '4EDental.com',
-    background: <Globe className="top-10 left-48" />,
-    className: 'lg:col-start-1 lg:col-end-2 lg:row-start-4 lg:row-end-7',
-  },
+  Icon: GlobeIcon,
+  name: 'AI SaaS Dental Imaging Platform',
+  description:
+    'Revolutionizing dental imaging with AI-powered diagnostics that detect issues with 98% accuracy, helping dentists provide better care and reduce misdiagnosis.',
+  href: '/contact-us',
+  cta: 'Learn more',
+  badgeClass: 'bg-red-500 dark:bg-red-600',
+  badgeLabel: 'Healthcare',
+  BadgeIcon: HeartIcon,
+  companyName: '4EDental.com',
+  background: <Globe className="absolute right-0 bottom-0 opacity-30" />,
+  className: 'lg:col-start-2 lg:col-end-3 lg:row-start-1 lg:row-end-4',
+},
+
+
 ];
 
 export default function BentoDemo() {
